7f0137824d1505dff5a472a53a5548fa4b688257
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-11 10:03:41 +0100

    WIP

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,30 @@
 return {
-	{ 'echasnovski/mini.basics', version = '*', lazy = false, opts = { mappings = { windows = true } } },
-	{ 'echasnovski/mini.starter', version = '*', opts = true },
-	{ 'echasnovski/mini.statusline', version = '*', opts = true },
-	{ 'echasnovski/mini.files', version = '*', depends = { 'echasnovski/mini.icon', version = '*', opts = true }, opts = true },
+	{ 'echasnovski/mini.basics', version = false, lazy = false, opts = { mappings = { windows = true } } },
+	{ 'echasnovski/mini.starter', version = false, opts = true },
+	{ 'echasnovski/mini.icons', version = false, opts = true },
+	{ 'echasnovski/mini.statusline', version = false, opts = true },
+	{ 'echasnovski/mini.clue', version = false, opts = { 
+		window = {
+    			delay = 500,
+    			config = { width = "auto", border = "single" },
+  		},
+		triggers = { 
+			-- leader triggers
+			{ mode = 'n', keys = '<Leader>' },
+			{ mode = 'x', keys = '<Leader>' },
+
+	    		-- Built-in completion
+	    		{ mode = 'i', keys = '<C-x>' },
+
+	    		-- `g` key
+	    		{ mode = 'n', keys = 'g' },
+	    		{ mode = 'x', keys = 'g' },
+			},
+			
+			-- `z` key
+    			{ mode = 'n', keys = 'z' },
+    			{ mode = 'x', keys = 'z' },
+		} 
+	},
+	{ 'echasnovski/mini.files', version = false, depends = { 'echasnovski/mini.icon', version = false, opts = true }, opts = true },
 }
