9709d2187ab3765811f6ae10ecbe8f3c0fb36839
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-23 22:04:59 +0100

    chore: add some comments in init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- Keymaps MUST be loaded before lazy.nvim
 require("config.keymaps")
+
+-- Options SHOULD be loaded before lazy.nvim
 require("config.options")
+
+-- Setup lazy.nvim plugin manager
 require("config.lazy")
