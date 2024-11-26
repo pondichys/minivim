a380836368e3df6ec316254c06a54df6f9657e96
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-26 11:00:55 +0100

    feat: add keymap to delete buffer with mini.buffremove
    Also new formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,14 @@
 local keymap = vim.keymap -- for conciseness
 
 -- Move between buffers
-keymap.set('n', '<S-h>', '<cmd>bprev<cr>', { desc = "Move to previous buffer" })
-keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = "Move to next buffer" })
+keymap.set('n', '<S-h>', '<cmd>bprev<cr>', { desc = 'Move to previous buffer' })
+keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Move to next buffer' })
+
+-- Delete buffers using mini.buffremove
+keymap.set('n', '<leader>bd', '<cmd> lua MiniBufremove.delete()<cr>', { desc = 'Delete buffer' })
 
 -- Window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' }) -- split window vertically
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' }) -- split window horizontally
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' }) -- make split windows equal width & height
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' }) -- close current split window
